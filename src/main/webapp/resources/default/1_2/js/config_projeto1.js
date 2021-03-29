13ec86797a3106ec1067a4332fd18eaf5fc720c6
Max <maiadefreitas@hotmail.com>
2021-03-29 20:48:58 -0300

    commit canvas

--- src/main/webapp/resources/default/1_2/js/config_projeto1.js
+++ src/main/webapp/resources/default/1_2/js/config_projeto1.js
@@ -1,132 +1,102 @@
-﻿var scene, camera, renderer;
-var geometry, material, cube;
-var keydown = [];
-var id;
+﻿(()=> {
+    'use stric'
+	var control = { x: 0, y: 0, z: 500}
+	var scene, camera, renderer;
+	var key = { event: " ", status: true};
 
-window.onload = function() {
+	window.onload = BuilderGamer
+	document.addEventListener("keydown", CaptureKeyDown)
+	document.addEventListener("keyup", CaptureKeyUp)
+	window.onload = startup
+	function BuilderGamer(){
+	
+		SceneBuild()
+		CubeMaker()
+	}
+	
+	function SceneBuild(){
+		scene = new THREE.Scene();
+	    camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+	    renderer = new THREE.WebGLRenderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	    document.body.appendChild(renderer.domElement); 
+	   	camera.position.z = control.z
+	   	animate()
+	}
+	
+	function CubeMaker(x=100,y=100,z=100, color='blue'){
+			
+	    let geometry = new THREE.BoxGeometry(x,y,z);
+	    let material = new THREE.MeshBasicMaterial({ color: color});
+	    let cube = new THREE.Mesh(geometry, material);
+	    
+	    scene.add(cube);
+				
+	}
+	
+	function CaptureKeyDown(event){
+		key.event = event.key
+		key.status = true
+		  
+	}
+	
+	function CaptureKeyUp(event){
+		key.event = event.key
+		key.status = false
+	}
+	
+	function animate(){
+		let joy = JoyStick()
+			
+		if (key.status){
+		
+			 camera.position.z += joy.z
+			 camera.position.x += joy.x
+			
+			 console.log("animate: ", key,joy)
+		}
+	   	renderer.render(scene, camera);
+	   	requestAnimationFrame(animate);
+  	
+	}
 
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer();
-    
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement); 
-    
-    geometry = new THREE.BoxGeometry(100,100,100);
-    material = new THREE.MeshBasicMaterial({ color: 'blue'});
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+	function JoyStick(){
+	
+		if(key.status){
+		
+			switch(key.event){
+				case 'ArrowUp':
+					control.z -=10
+					break
+				case 'ArrowDown':
+					control.z +=10
+					break
+				case 'ArrowRight':
+					control.x +=10
+					break
+				case 'ArrowLeft':
+					control.x -=10
+					break
+				default:
+			}
+			
+		} 
+	
+		return control 
+	}
 
-   camera.position.z = 500;
-           
-        window.onkeydown = function(e)
-        {
-        keydown[e.key]=true;
-        }
-        
-        window.onkeyup = function(e)
-        {
-        keydown[e.key]=false;  
-        }
-        
-        var animate = function()
-        {	
-        
-        requestAnimationFrame(animate);
-        
-        if(keydown["ArrowUp"]) camera.position.z-=10;
-         if(keydown["ArrowDown"]) camera.position.z+=10;
-          if(keydown["ArrowRight"]) camera.position.x+=10;
-           if(keydown["ArrowLeft"]) camera.position.x-=10;
-                
-         renderer.render(scene, camera);
-             
-        }
-       
-             
-        animate();
- 
-       }
 
-function mDown(obj)
-{
-
- animate2();
-
+function startup() {
+  var el = document.getElementById("canvas");
+  el.addEventListener("touchstart", handleStart, false);
+  el.addEventListener("touchend", handleEnd, false);
+  el.addEventListener("touchcancel", handleCancel, false);
+  el.addEventListener("touchmove", handleMove, false);
 }
 
-function mUp(obj)
-{
-
-cancelAnimationFrame(id);
-
-}
-
-
-        var animate2 = function()
-        {	
-        
-        id = requestAnimationFrame(animate2);
-        
-        camera.position.z-=10;
-                         
-         renderer.render(scene, camera);
-             
-        }
+document.addEventListener("DOMContentLoaded", startup);
 
 
 
+})()
 
-
-
-//--------------------------------- Explicação --------------------------//
-
-//    * Estanciando objeto "THREE.Scene()". Objeto responsavel por criar a cena.
-//scene = new THREE.Scene();
-
-//    * Estanciando objeto "THREE.PerspectiveCamera". Objeto responsavel por criar camera.
-//    * Atributo ".position" e logo após o eixo utilizado ".z" porém existem 3 eixos " x y z " cada um com sua caracteristica no plano cartesiano.
-//camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-//camera.position.z = 1000;
-
-//    * Estanciando objeto 3D primitivo. Neste caso é feito uma estancia de um Cubo usando o seguinte objeto "THREE.BoxGeometry". E suas dimensões logo após.
-//geometry = new THREE.BoxGeometry(200, 200, 200);
-
-//    * Material que sera aplicado no objeto neste caso as cores. Estanciando o objeto "THREE.MeshBasicMaterial". Wireframe exibe somente o "esqueleto" do objeto.
-//material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-
-//    * Criando objeto 3D com material aplicado, estanciando o objeto "THREE.Mesh()". Parametros: 1º var "geometry" , 2º var "material"  
-//mesh = new THREE.Mesh(geometry, material);
-//    * Adicionando o objeto na cena. Utilizando o atributo ".add" logo em seguida a var "mesh".
-//scene.add(mesh);
-
-//    * Estancia do objeto "THREE.WebGLRenderer()" responsavel por renderizar a cena e o objeto.
-//renderer = new THREE.WebGLRenderer();
-
-//    * Atributo ".setSize" define a área de renderização. Neste caso sera as dimensões da janela do browser "window.innerWidth" e "window.innerHeight"
-//renderer.setSize(window.innerWidth, window.innerHeight);
-
-//    * Atribuindo local de renderização neste caso o elemento "body".
-//document.body.appendChild(renderer.domElement);
-//                       OU
-//    * Atribuindo local de renderização pela tag "body" = <body>
-//document.getElementsByTagName('body')[0].appendChild( renderer.domElement );
-
-//    * Função usada para animar o objeto
-//function animate() {
-
-//    executando recursivamente
-//    requestAnimationFrame(animate);
-
-//    * Alterando os valores de rotação nos eixos para que ocorra uma animação
-//    mesh.rotation.x += 0.01;
-//    mesh.rotation.y += 0.02;
-
-//    * Renderização de cena e camera. Usado para renderizar o resultado final.
-//    renderer.render(scene, camera);
-
-//}
-
-//  * Executando função!
-//  animate();
-//}
